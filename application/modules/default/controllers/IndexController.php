f79e1534912899df3e12b9038ccf6032680ec529
Volodymyr <Volodymyr@localhost>
2010-10-08 13:31:58 +0300

    initial commit

---
+++ application/modules/default/controllers/IndexController.php
@@ -0,0 +1,8 @@
+<?php
+class IndexController extends Abstract_Controller_FrontendController {
+	
+	public function indexAction(){
+		$this->view->layouts['center']["index_info_block"] = array('inc/seo', 100);
+	}
+
+}
---
+++ library/ArOn/Crud/Form/Field/Date.php
@@ -0,0 +1,49 @@
+<?php
+class ArOn_Crud_Form_Field_Date extends ArOn_Crud_Form_Field {
+
+	protected $startDate;
+	protected $endDate;
+	protected $format;
+
+	function __construct($name, $title = null, $description = null, $startDate = null, $endDate = null, $required = null, $notEdit = false, $format = 'YYYY-MM-DD') {
+
+		$this->startDate = $startDate;
+		$this->endDate = $endDate;
+		$this->format = $format;
+		parent::__construct ( $name, $title, $description, $required, $notEdit );
+	}
+
+	public function updateField() {
+		parent::updateField ();
+		$this->element->setValidators ( array (array ('NotEmpty', true ), new Zend_Validate_Date ( $this->format, 'en_US' ) ) );
+		if (null != $this->startDate) {
+			$validator = new Zend_Validate_GreaterThan ( $this->startDate );
+			$this->element->addValidator ( $validator );
+		}
+		if (null != $this->endDate) {
+			$validator = new Zend_Validate_LessThan ( $this->endDate );
+			$this->element->addValidator ( $validator );
+		}
+		$this->element->helper = 'GridFormCalendar';
+	}
+
+	public function getInsertData() {
+		if (! $this->saveInDataBase)
+		return false;
+		$value = $this->getValue ();
+		$value = str_replace ( "-", "/", $value );
+		$value = date ( 'Y-m-d', strtotime ( $value ) );
+
+		$data = array ();
+		$data ['model'] = 'default';
+		$data ['data'] = array ('key' => $this->getName (), 'value' => $value );
+
+		return $data;
+	}
+
+	public function setValue($value) {
+		$value = str_replace ( "-", "/", $value );
+		$value = date ( 'Y-m-d', strtotime ( $value ) );
+		return parent::setValue ( $value );
+	}
+}
