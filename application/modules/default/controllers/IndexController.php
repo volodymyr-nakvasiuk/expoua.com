00662e5efecba42a7fb6d64602c41a82c48f5d9e
Anton <Anton@localhost>
2010-11-25 22:05:15 +0200

    Register ? ???? ??????????? Client

--- application/modules/default/controllers/IndexController.php
+++ application/modules/default/controllers/IndexController.php
@@ -6,8 +6,9 @@ class IndexController extends Abstract_Controller_FrontendController {
 //		$this->view->layouts['center']['center_companies_news_box'] = array('inc/center/company_news', 100);
 //		$this->view->layouts['extra']['extra_companies_goods_box'] = array('inc/extra/goods_slider', 100);
 		$this->view->layouts['right']['right_advert_box'] = array('inc/right/advert', 100);
-        $this->view->layouts['body']['client_anons'] = array('inc/body/client_anons', 100);
-        $this->view->layouts['left']['client_menu'] = array('inc/left/client_menu', 100);
+        $this->view->layouts['body']['client_reg'] = array('inc/body/register', 100);
+//      $this->view->layouts['body']['client_anons'] = array('inc/body/client_anons', 100);
+//      $this->view->layouts['left']['client_menu'] = array('inc/left/client_menu', 100);
 //		$this->view->layouts['body']['body_events_accordion_box'] = array('inc/body/events_accordion', 100);
 //		$this->view->layouts['body']['body_companies_box'] = array('inc/body/companies', 100);
 	}
