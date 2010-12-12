cb51d827988ed5d23564bebf4c377869e876c8e4
Anton <Anton@localhost>
2010-12-12 15:19:28 +0200

    CLIENT

--- application/modules/default/controllers/IndexController.php
+++ application/modules/default/controllers/IndexController.php
@@ -8,10 +8,10 @@ class IndexController extends Abstract_Controller_FrontendController {
 //        $this->view->layouts['right']['right_advert_box'] = array('inc/right/advert', 100);
 //        $this->view->layouts['body']['client_reg'] = array('inc/body/register', 100);
         $this->view->layouts['right']['right_advert_box'] = array('inc/right/advert', 100);
-        $this->view->layouts['body']['companies_search_box'] = array('inc/body/companies_search', 100);
-        $this->view->layouts['center']['company_news'] = array('inc/center/top_companies', 100);
-//      $this->view->layouts['body']['client_anons'] = array('inc/body/client_anons', 100);
-//      $this->view->layouts['left']['client_menu'] = array('inc/left/client_menu', 100);
+        //$this->view->layouts['body']['companies_search_box'] = array('inc/body/companies_search', 100);
+        //$this->view->layouts['center']['company_news'] = array('inc/center/top_companies', 100);
+        $this->view->layouts['body']['user_add_about_company'] = array('inc/body/user_add_about_company', 100);
+        $this->view->layouts['left']['client_menu'] = array('inc/left/client_menu', 100);
 //		$this->view->layouts['body']['body_events_accordion_box'] = array('inc/body/events_accordion', 100);
 //		$this->view->layouts['body']['body_companies_box'] = array('inc/body/companies', 100);
 	}
