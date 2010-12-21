79570c79e8febcfd7084b609057bc27889d5b448
Volodymyr WORK <Volodymyr WORK@localhost>
2010-12-21 19:16:06 +0200

    cache killing

--- admin/application/templates_c/Backend/%%6F/6F8/6F8AF676%%delete.tpl.php
+++
@@ -1,7 +0,0 @@
-<?php /* Smarty version 2.6.18, created on 2010-12-21 14:59:14
-         compiled from controllers/admin_acl_admins/delete.tpl */ ?>
-<?php $_smarty_tpl_vars = $this->_tpl_vars;
-$this->_smarty_include(array('smarty_include_tpl_file' => "common/generalDelete.tpl", 'smarty_include_vars' => array()));
-$this->_tpl_vars = $_smarty_tpl_vars;
-unset($_smarty_tpl_vars);
- ?>
