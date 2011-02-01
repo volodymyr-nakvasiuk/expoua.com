78232b58bf88ea88856754df4c0e1b2cbcd654fe
Volodymyr WORK <Volodymyr WORK@localhost>
2011-02-01 16:45:04 +0200

    events search right columns

--- application/modules/default/controllers/EventsController.php
+++ application/modules/default/controllers/EventsController.php
@@ -32,6 +32,9 @@ class EventsController extends Abstract_Controller_FrontendController {
 		$this->view->layoutsData['center']['center_events_news_box'] = $grid->getData();
 		$this->view->layouts['center']['center_events_news_box'] = array('inc/center/event_news', 100);
 
+		$this->view->layouts['center']['center_events_by_countries'] = array('inc/center/events_by_countries', 100);
+		$this->view->layouts['center']['center_events_by_categories'] = array('inc/center/events_by_categories', 100);
+
 		$tool = new Tools_Banner('side250', $this->moduleName, true, $this->brandsCategoryId, 1);
 		$this->view->banners['250x250']['right_banners_250x250_box'] = array($tool->getData(), 100);
 		$tool->updateStat();
